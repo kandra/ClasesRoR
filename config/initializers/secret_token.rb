bd1ffccaeb6a95dc2778acc0a783cb8df921520e
Karen Da Cruz <me@karendacruz.com>
2011-12-13 19:50:37 -0500

    Mi primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrimeraApp::Application.config.secret_token = 'cc0a8dd04bf4c702865fad1596da394b543a201336f6c9ea2d57f59809c80674f51e4087469f4995a9fc07b308852af86362accf6c23997424b41272a4e4081b'
